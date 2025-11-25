46f4e0d29adfc19a06f45c18875f6fc33ace57f5
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-24 16:51:52 -0800

    feat: notification item on bottom bar

--- src/webapp/components/navigation/bottomBar/BottomBar.tsx
+++ src/webapp/components/navigation/bottomBar/BottomBar.tsx
@@ -4,6 +4,7 @@ import { LuLibrary } from 'react-icons/lu';
 import { MdOutlineEmojiNature } from 'react-icons/md';
 import BottomBarItem from '../bottomBarItem/BottomBarItem';
 import useMyProfile from '@/features/profile/lib/queries/useMyProfile';
+import { RiNotification2Line } from 'react-icons/ri';
 
 export default function BottomBar() {
   const { data: profile } = useMyProfile();
@@ -13,6 +14,7 @@ export default function BottomBar() {
       <Group align="start" justify="space-around" gap={'lg'} h={'100%'}>
         <BottomBarItem href="/home" icon={LuLibrary} />
         <BottomBarItem href="/explore" icon={MdOutlineEmojiNature} />
+        <BottomBarItem href="/notifications" icon={RiNotification2Line} />
         <BottomBarItem
           href={`/profile/${profile.handle}/cards`}
           icon={FaRegNoteSticky}
