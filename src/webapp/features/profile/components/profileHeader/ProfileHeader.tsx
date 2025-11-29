7eb758a621abb581bdbafcb41c9c253a572e972e
Pouria Delfanazari <pdelfan@gmail.com>
2025-11-28 16:46:34 -0800

    feat: decrease bluesky profile link size

--- src/webapp/features/profile/components/profileHeader/ProfileHeader.tsx
+++ src/webapp/features/profile/components/profileHeader/ProfileHeader.tsx
@@ -75,6 +75,7 @@ export default async function ProfileHeader(props: Props) {
               href={`https://bsky.app/profile/${profile.handle}`}
               target="_blank"
               variant="light"
+              size="xs"
               radius={'xl'}
               color={'gray'}
               leftSection={<FaBluesky />}
