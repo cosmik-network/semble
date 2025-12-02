cfe2deedb5ea4891a0c831a14155550479e72353
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-01 16:14:38 -0800

    feat: "no notifications" message

--- src/webapp/features/notifications/containers/notificationsContainer/NotificationsContainer.tsx
+++ src/webapp/features/notifications/containers/notificationsContainer/NotificationsContainer.tsx
@@ -1,10 +1,22 @@
-import { Container, Stack, Title } from '@mantine/core';
+import { Container, Stack, Title, Text } from '@mantine/core';
 
 export default function NotificationsContainer() {
+  const count = 0;
+
   return (
     <Container p="xs" size="xl">
-      <Stack gap="xl">
+      <Stack>
         <Title order={1}>Notifications</Title>
+
+        {count === 0 ? (
+          <Stack align="center" gap="xs">
+            <Text fz="h3" fw={600} c="gray">
+              No notifications right now — but we're on the lookout!
+            </Text>
+          </Stack>
+        ) : (
+          <></>
+        )}
       </Stack>
     </Container>
   );
