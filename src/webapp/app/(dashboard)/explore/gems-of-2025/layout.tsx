2ee7dbcf5fa5af979a986efc6780d78b031c951f
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-11 14:45:54 -0800

    feat: gems feed metadata

---
+++ src/webapp/app/(dashboard)/explore/gems-of-2025/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Explore — Gems of 2025',
+  description: 'Top picks from our community',
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default async function Layout(props: Props) {
+  return props.children;
+}
