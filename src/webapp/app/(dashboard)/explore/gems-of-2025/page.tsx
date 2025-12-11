f5649a0d72ca04b7b88b7b2209fc5a63050938f4
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-11 15:12:29 -0800

    feat: remove gems banner from feeds

--- src/webapp/app/(dashboard)/explore/gems-of-2025/page.tsx
+++ src/webapp/app/(dashboard)/explore/gems-of-2025/page.tsx
@@ -1,11 +1,5 @@
 import GemsFeedContainer from '@/features/feeds/containers/gemsFeedContainer/GemsFeedContainer';
-import GemsOfYearBanner from '@/features/collections/components/gemsOfYearBanner/GemsOfYearBanner';
 
 export default function Page() {
-  return (
-    <>
-      <GemsOfYearBanner />
-      <GemsFeedContainer />
-    </>
-  );
+  return <GemsFeedContainer />;
 }
