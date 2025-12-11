da0db16f867e6e54608e208fab08fb3641fdf59d
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-11 15:12:07 -0800

    feat: gems feed tabs

--- src/webapp/app/(dashboard)/explore/gems-of-2025/layout.tsx
+++ src/webapp/app/(dashboard)/explore/gems-of-2025/layout.tsx
@@ -1,4 +1,10 @@
+import BackButton from '@/components/navigation/backButton/BackButton';
+import Header from '@/components/navigation/header/Header';
 import type { Metadata } from 'next';
+import { Fragment } from 'react';
+import { verifySessionOnServer } from '@/lib/auth/dal.server';
+import FeedTabs from '@/features/feeds/components/feedTabs/FeedTabs';
+import { Paper } from '@mantine/core';
 
 export const metadata: Metadata = {
   title: 'Explore — Gems of 2025',
@@ -10,5 +16,22 @@ interface Props {
 }
 
 export default async function Layout(props: Props) {
-  return props.children;
+  const session = await verifySessionOnServer();
+
+  return (
+    <Fragment>
+      <Header title="Explore (Gems of 2025)">
+        {session ? (
+          <BackButton href="/home">Home</BackButton>
+        ) : (
+          <BackButton href="/">Learn more</BackButton>
+        )}
+      </Header>
+      <Paper pos={'sticky'} top={55} style={{ zIndex: 1 }}>
+        <FeedTabs />
+      </Paper>
+
+      {props.children}
+    </Fragment>
+  );
 }
