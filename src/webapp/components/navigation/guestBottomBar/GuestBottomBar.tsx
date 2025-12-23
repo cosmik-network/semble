b77f975c6957ccd4c7d27eed43791ba435559f72
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-23 13:22:30 -0800

    fix: home item visible on guest bottom bar

--- src/webapp/components/navigation/guestBottomBar/GuestBottomBar.tsx
+++ src/webapp/components/navigation/guestBottomBar/GuestBottomBar.tsx
@@ -1,5 +1,4 @@
 import { AppShellFooter, Avatar, Group } from '@mantine/core';
-import { LuLibrary } from 'react-icons/lu';
 import { MdOutlineEmojiNature } from 'react-icons/md';
 import BottomBarItem from '../bottomBarItem/BottomBarItem';
 
@@ -7,7 +6,6 @@ export default function GuestBottomBar() {
   return (
     <AppShellFooter px={'sm'} pb={'lg'} py={'xs'} hiddenFrom="sm">
       <Group align="start" justify="space-around" gap={'lg'} h={'100%'}>
-        <BottomBarItem href="/home" title="Home" icon={LuLibrary} />
         <BottomBarItem
           href="/explore"
           title="Explore"
