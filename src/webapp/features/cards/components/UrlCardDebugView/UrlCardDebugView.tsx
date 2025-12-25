f61b2ea9fe1ddfa5e0da5b93d2ad0bd142cfc75c
Pouria Delfanazari <pdelfan@gmail.com>
2025-12-25 12:47:47 -0800

    Update UrlCardDebugView.tsx

--- src/webapp/features/cards/components/UrlCardDebugView/UrlCardDebugView.tsx
+++ src/webapp/features/cards/components/UrlCardDebugView/UrlCardDebugView.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import {
-  CodeHighlightControl,
-  CodeHighlightTabs,
-} from '@mantine/code-highlight';
-import { TbBinaryTreeFilled } from 'react-icons/tb';
+import { CodeHighlightTabs } from '@mantine/code-highlight';
 import type { UrlCard, User } from '@/api-client';
 
 interface Props {
@@ -26,21 +22,6 @@ export default function UrlCardDebugView(props: Props) {
           code: JSON.stringify(props.cardAuthor, null, 2),
           language: 'json',
         },
-      ]}
-      controls={[
-        <CodeHighlightControl
-          key="pdsls"
-          component="a"
-          href={`https://pdsls.dev/at://${props.cardAuthor?.id}/${
-            process.env.NODE_ENV
-              ? 'network.cosmik.dev.card'
-              : 'network.cosmik.card'
-          }`}
-          target="_blank"
-          tooltipLabel="View cards on pdsls"
-        >
-          <TbBinaryTreeFilled fill="#76c4e5" />
-        </CodeHighlightControl>,
       ]}
       radius="md"
       withBorder
