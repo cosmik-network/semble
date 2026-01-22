22f4f8c593516545f0ad5dd4eb8cfc8973009a83
Pouria Delfanazari <pdelfan@gmail.com>
2026-01-21 16:06:16 -0800

    refactor: landing page footer

--- src/webapp/app/page.tsx
+++ src/webapp/app/page.tsx
@@ -1,5 +1,4 @@
 import {
-  ActionIcon,
   SimpleGrid,
   Image,
   Text,
@@ -11,25 +10,21 @@ import {
   Box,
   Center,
   Group,
-  Anchor,
   Badge,
 } from '@mantine/core';
-import { FaBluesky, FaGithub, FaDiscord } from 'react-icons/fa6';
 import { BiRightArrowAlt } from 'react-icons/bi';
-import { RiArrowRightUpLine } from 'react-icons/ri';
 import BG from '@/assets/semble-bg.webp';
 import DarkBG from '@/assets/semble-bg-dark.png';
-import CosmikLogo from '@/assets/cosmik-logo-full.svg';
-import CosmikLogoWhite from '@/assets/cosmik-logo-full-white.svg';
 import CurateIcon from '@/assets/icons/curate-icon.svg';
 import CommunityIcon from '@/assets/icons/community-icon.svg';
 import DBIcon from '@/assets/icons/db-icon.svg';
 import BigPictureIcon from '@/assets/icons/big-picture-icon.svg';
-import TangledIcon from '@/assets/icons/tangled-icon.svg';
 import SembleLogo from '@/assets/semble-logo.svg';
 import Link from 'next/link';
 import AnimatedTitle from '@/components/landing/animatedTitle/AnimatedTitle';
 import IosShortcutButton from '@/components/landing/iosShortcutButton/IosShortcutButton';
+import { Fragment } from 'react';
+import Footer from '@/components/landing/footer/Footer';
 
 export default async function Page() {
   const fadeStyle = {
@@ -68,7 +63,7 @@ export default async function Page() {
 
 function Content() {
   return (
-    <>
+    <Fragment>
       <script async src="https://tally.so/widgets/embed.js" />
       <Container size="xl" p="md" my="auto">
         <Group justify="space-between">
@@ -189,140 +184,6 @@ function Content() {
           </Stack>
         </Container>
       </Center>
-    </>
-  );
-}
-
-function Footer() {
-  return (
-    <Box component="footer" px="md" py="xs" mt="xl" pos="relative">
-      <Stack align="center" gap="xs">
-        <Group gap="0">
-          <ActionIcon
-            component="a"
-            href="https://bsky.app/profile/cosmik.network"
-            target="_blank"
-            variant="subtle"
-            color="dark.2"
-            radius="xl"
-            size="xl"
-            m={0}
-          >
-            <FaBluesky size={22} />
-          </ActionIcon>
-          <ActionIcon
-            component="a"
-            href="https://tangled.org/@cosmik.network/semble"
-            target="_blank"
-            variant="subtle"
-            color="dark.2"
-            radius="xl"
-            size="xl"
-          >
-            <Image src={TangledIcon.src} alt="Tangled logo" w="auto" h={22} />
-          </ActionIcon>
-          <ActionIcon
-            component="a"
-            href="https://github.com/cosmik-network"
-            target="_blank"
-            variant="subtle"
-            color="dark.2"
-            radius="xl"
-            size="xl"
-          >
-            <FaGithub size={22} />
-          </ActionIcon>
-          <ActionIcon
-            component="a"
-            href="https://discord.gg/SHvvysb73e"
-            target="_blank"
-            variant="subtle"
-            color="dark.2"
-            radius="xl"
-            size="xl"
-          >
-            <FaDiscord size={22} />
-          </ActionIcon>
-        </Group>
-        <Group gap={'xs'} justify="center">
-          <Button
-            component="a"
-            href="https://blog.cosmik.network"
-            target="_blank"
-            variant="light"
-            color="dark.1"
-            fw={600}
-            rightSection={<RiArrowRightUpLine />}
-          >
-            Follow our blog for updates
-          </Button>
-          <Button
-            component="a"
-            href="https://docs.cosmik.network/semble"
-            target="_blank"
-            variant="light"
-            color="dark.1"
-            fw={600}
-            rightSection={<RiArrowRightUpLine />}
-          >
-            Semble Docs
-          </Button>
-        </Group>
-        <Stack align="center" gap="0">
-          <Text c="dark.1" fw={600} ta="center">
-            Made by &nbsp;
-            <Anchor
-              href="https://cosmik.network/"
-              target="_blank"
-              style={{ verticalAlign: 'middle' }}
-            >
-              <Box
-                component="span"
-                display="inline-flex"
-                style={{ verticalAlign: 'middle' }}
-              >
-                {/* light logo */}
-                <Image
-                  src={CosmikLogo.src}
-                  alt="Cosmik logo"
-                  w={92}
-                  h={28.4}
-                  darkHidden
-                />
-                {/* dark logo */}
-                <Image
-                  src={CosmikLogoWhite.src}
-                  alt="Cosmik logo white"
-                  w={92}
-                  h={28.4}
-                  lightHidden
-                />
-              </Box>
-            </Anchor>
-            &nbsp;&nbsp;
-            <Text c="dark.1" fw={600} span>
-              with support from&nbsp;
-              <Anchor
-                href="https://coefficientgiving.org/"
-                target="_blank"
-                c="dark.2"
-                fw={600}
-              >
-                Coefficient Giving
-              </Anchor>{' '}
-              and{' '}
-              <Anchor
-                href="https://astera.org/"
-                target="_blank"
-                c="dark.2"
-                fw={600}
-              >
-                Astera
-              </Anchor>
-            </Text>
-          </Text>
-        </Stack>
-      </Stack>
-    </Box>
+    </Fragment>
   );
 }
