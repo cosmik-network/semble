a92686ae76a6ff0ff3462d2476ef708267ebe32e
Pouria Delfanazari <pdelfan@gmail.com>
2026-01-26 14:43:20 -0800

    feat: search link on profile cards tab

--- src/webapp/features/cards/containers/cardsContainer/CardsContainer.tsx
+++ src/webapp/features/cards/containers/cardsContainer/CardsContainer.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import { Container, Stack } from '@mantine/core';
+import { Button, Container, Stack } from '@mantine/core';
 import { Suspense } from 'react';
 import CardsContainerContent from '../cardsContainerContent/CardsContainerContent';
 import CardsContainerContentSkeleton from '../cardsContainerContent/Skeleton.CardsContainerContent';
 import { CardFilters } from '@/features/cards/components/cardFilters/CardFilters';
+import Link from 'next/link';
+import { IoSearch } from 'react-icons/io5';
 
-export default function CardsContainer({ handle }: { handle: string }) {
+interface Props {
+  handle: string;
+}
+
+export default function CardsContainer(props: Props) {
   return (
     <Container p="xs" size="xl">
       <Stack>
@@ -14,10 +20,19 @@ export default function CardsContainer({ handle }: { handle: string }) {
           <CardFilters.SortSelect />
           <CardFilters.TypeFilter />
           <CardFilters.ViewToggle />
+          <Button
+            component={Link}
+            href={`/search/cards?handle:${props.handle}`}
+            variant="light"
+            color="gray"
+            rightSection={<IoSearch />}
+          >
+            Search
+          </Button>
         </CardFilters.Root>
 
         <Suspense fallback={<CardsContainerContentSkeleton />}>
-          <CardsContainerContent handle={handle} />
+          <CardsContainerContent handle={props.handle} />
         </Suspense>
       </Stack>
     </Container>
