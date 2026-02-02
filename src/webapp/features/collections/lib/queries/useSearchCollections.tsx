70703ac774f0c8e2eae4fd79b7787c9605aecea1
Pouria Delfanazari <pdelfan@gmail.com>
2026-02-02 11:23:26 -0800

    refactor: collection search

--- src/webapp/features/collections/lib/queries/useSearchCollections.tsx
+++ src/webapp/features/collections/lib/queries/useSearchCollections.tsx
@@ -1,33 +1,39 @@
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { searchCollections } from '../dal';
 import { collectionKeys } from '../collectionKeys';
-import { CollectionSortField } from '@semble/types';
-
+import {
+  CollectionSortField,
+  CollectionAccessType,
+  GetCollectionsResponse,
+} from '@semble/types';
 interface Props {
   searchText: string;
   limit?: number;
   sortBy?: CollectionSortField;
-  handleOrDid?: string;
+  accessType?: CollectionAccessType;
+  identifier?: string;
 }
 
 export default function useSearchCollections(props: Props) {
   const limit = props?.limit ?? 15;
 
-  return useSuspenseInfiniteQuery({
+  return useSuspenseInfiniteQuery<GetCollectionsResponse>({
     queryKey: collectionKeys.search(
       props.searchText,
       props.limit,
       props.sortBy,
-      props.handleOrDid,
+      props.accessType,
+      props.identifier,
     ),
     initialPageParam: 1,
     queryFn: ({ pageParam }) =>
       searchCollections({
         searchText: props.searchText,
         limit,
-        page: pageParam,
+        page: pageParam as number,
         collectionSortBy: props.sortBy,
-        handleOrDid: props.handleOrDid,
+        accessType: props.accessType,
+        identifier: props.identifier,
       }),
     getNextPageParam: (lastPage) => {
       return lastPage.pagination.hasMore
