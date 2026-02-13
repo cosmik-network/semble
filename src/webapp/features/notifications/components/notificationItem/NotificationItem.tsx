db5a3b6720a464de703a0d2d42c1543a0cd43bd3
Wesley Finck <wesleyfinck@gmail.com>
2026-02-13 15:26:43 -0800

    fix webapp build error

--- src/webapp/features/notifications/components/notificationItem/NotificationItem.tsx
+++ src/webapp/features/notifications/components/notificationItem/NotificationItem.tsx
@@ -23,18 +23,20 @@ export default function NotificationItem(props: Props) {
           createdAt={props.item.createdAt}
           type={props.item.type}
         />
-        <UrlCard
-          id={props.item.card.id}
-          url={props.item.card.url}
-          uri={props.item.card.uri}
-          note={props.item.card.note}
-          cardAuthor={props.item.card.author}
-          cardContent={props.item.card.cardContent}
-          urlLibraryCount={props.item.card.urlLibraryCount}
-          urlIsInLibrary={props.item.card.urlInLibrary}
-          authorHandle={props.item.user.handle}
-          viaCardId={props.item.card.id}
-        />
+        {props.item.card && (
+          <UrlCard
+            id={props.item.card.id}
+            url={props.item.card.url}
+            uri={props.item.card.uri}
+            note={props.item.card.note}
+            cardAuthor={props.item.card.author}
+            cardContent={props.item.card.cardContent}
+            urlLibraryCount={props.item.card.urlLibraryCount}
+            urlIsInLibrary={props.item.card.urlInLibrary}
+            authorHandle={props.item.user.handle}
+            viaCardId={props.item.card.id}
+          />
+        )}
       </Stack>
     </Indicator>
   );
