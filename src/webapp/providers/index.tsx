5541ec4069f272d601d5f4ba43e7a7bba85a7f8d
Jasmine Zou <jasmine_pyz@icloud.com>
2026-02-17 13:44:04 -0800

    added posthog to provider

--- src/webapp/providers/index.tsx
+++ src/webapp/providers/index.tsx
@@ -1,25 +1,44 @@
 'use client';
 
 import { AuthProvider } from '@/hooks/useAuth';
+import { useEffect } from 'react';
 import MantineProvider from './mantine';
 import TanStackQueryProvider from './tanstack';
 import { NavbarProvider } from './navbar';
 import { SettingsProvider } from './settings';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Providers(props: Props) {
+  useEffect(() => {
+    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+
+    if (!posthogKey) {
+      console.warn('PostHog key not found in environment variables');
+      return;
+    }
+
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: '2026-01-30',
+    });
+  }, []);
+
   return (
-    <TanStackQueryProvider>
-      <AuthProvider>
-        <MantineProvider>
-          <SettingsProvider>
-            <NavbarProvider>{props.children}</NavbarProvider>
-          </SettingsProvider>
-        </MantineProvider>
-      </AuthProvider>
-    </TanStackQueryProvider>
+    <PostHogProvider client={posthog}>
+      <TanStackQueryProvider>
+        <AuthProvider>
+          <MantineProvider>
+            <SettingsProvider>
+              <NavbarProvider>{props.children}</NavbarProvider>
+            </SettingsProvider>
+          </MantineProvider>
+        </AuthProvider>
+      </TanStackQueryProvider>
+    </PostHogProvider>
   );
 }
