c6c64f6809f3f3d9f02f3106a594a97baac32991
pdelfan <pdelfan@gmail.com>
2026-02-20 13:56:00 -0800

    fix: collection following container

--- src/webapp/features/follows/containers/followingCollectionsContainer/Skeleton.FollowingCollectionsContainer.tsx
+++ src/webapp/features/follows/containers/followingCollectionsContainer/Skeleton.FollowingCollectionsContainer.tsx
@@ -1,12 +1,14 @@
 import CollectionCardSkeleton from '@/features/collections/components/collectionCard/Skeleton.CollectionCard';
-import { SimpleGrid } from '@mantine/core';
+import { Container, SimpleGrid } from '@mantine/core';
 
 export default function FollowingCollectionsContainerSkeleton() {
   return (
-    <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }} spacing="xs">
-      {Array.from({ length: 4 }).map((_, i) => (
-        <CollectionCardSkeleton key={i} />
-      ))}
-    </SimpleGrid>
+    <Container p={'xs'} size={'xl'}>
+      <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }} spacing="xs">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <CollectionCardSkeleton key={i} />
+        ))}
+      </SimpleGrid>
+    </Container>
   );
 }
