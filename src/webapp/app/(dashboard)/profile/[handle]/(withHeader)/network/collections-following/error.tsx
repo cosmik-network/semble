9fd8bfe7cd4443b8b504633285e46110790e5316
pdelfan <pdelfan@gmail.com>
2026-02-24 17:39:16 -0800

    feat: following collections error page

---
+++ src/webapp/app/(dashboard)/profile/[handle]/(withHeader)/network/collections-following/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import FollowingCollectionsContainerError from '@/features/follows/containers/followingCollectionsContainer/Error.FollowingCollectionsContainer';
+
+export default function Error() {
+  return <FollowingCollectionsContainerError />;
+}
