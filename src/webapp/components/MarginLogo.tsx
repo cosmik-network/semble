1ae67fbcca5c1befc114463d432fcce912a8e5b0
Wesley Finck <wesleyfinck@gmail.com>
2026-02-25 20:56:17 -0800

    margin logo update

--- src/webapp/components/MarginLogo.tsx
+++ src/webapp/components/MarginLogo.tsx
@@ -22,12 +22,12 @@ export default function MarginLogo({
       width={size}
       height={size}
       viewBox="0 0 265 231"
-      fill="#6366f1"
+      fill="#027bff"
       xmlns="http://www.w3.org/2000/svg"
       style={{ display: 'inline-block', verticalAlign: 'middle' }}
     >
       <path d="M0 230 V0 H199 V65.7156 H149.5 V115.216 H182.5 L199 131.716 V230 Z" />
-      <path d="M215 214.224 V230 H264.5 V0 H215.07 V16.2242 H248.5 V214.224 H215 Z" />
+      <path d="M215 214.224 V230 H264.5 V0 H215 V16.2242 H248.5 V214.224 H215 Z" />
     </Box>
   );
 
