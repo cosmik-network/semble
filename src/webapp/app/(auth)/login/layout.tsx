632df9e484562472c5d333a0aca35a1bd7a032c4
pdelfan <pdelfan@gmail.com>
2026-02-26 10:48:48 -0800

    refactor: only load login form as a client component

--- src/webapp/app/(auth)/login/layout.tsx
+++ src/webapp/app/(auth)/login/layout.tsx
@@ -1,5 +1,21 @@
-import { Center, Container } from '@mantine/core';
+import {
+  Center,
+  Container,
+  Stack,
+  Image,
+  Badge,
+  Title,
+  Text,
+  Popover,
+  Anchor,
+  PopoverTarget,
+  Button,
+  PopoverDropdown,
+} from '@mantine/core';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import { IoMdHelpCircleOutline } from 'react-icons/io';
+import SembleLogo from '@/assets/semble-logo.svg';
 
 export const metadata: Metadata = {
   title: 'Log in — Semble',
@@ -14,7 +30,75 @@ export default async function Layout(props: Props) {
   return (
     <Container>
       <Center h={'100svh'} p={'sm'}>
-        {props.children}
+        <Stack gap={'xl'} align="center">
+          <Stack gap="xl" maw={300}>
+            <Stack gap={'xs'}>
+              <Stack align="center" gap={'xs'}>
+                <Image
+                  src={SembleLogo.src}
+                  alt="Semble logo"
+                  w={48}
+                  h={64.5}
+                  mx={'auto'}
+                />
+                <Badge size="sm">Alpha</Badge>
+              </Stack>
+              <Title order={1} ta="center">
+                Welcome back
+              </Title>
+            </Stack>
+            {props.children}
+            <Stack align="center" gap={0}>
+              <Text fw={500} c={'stone'}>
+                {"Don't have an account? "}
+                <Anchor href="/signup" fw={500}>
+                  Sign up
+                </Anchor>
+              </Text>
+              <Popover withArrow shadow="sm">
+                <PopoverTarget>
+                  <Button
+                    variant="transparent"
+                    size="md"
+                    fw={500}
+                    fs={'italic'}
+                    c={'gray'}
+                    rightSection={<IoMdHelpCircleOutline size={22} />}
+                  >
+                    How your Cosmik Network account works
+                  </Button>
+                </PopoverTarget>
+                <PopoverDropdown>
+                  <Text fw={500} ta="center" maw={380}>
+                    If you have a Bluesky account, you can sign in with it; no
+                    new account is needed. In future, you will have the option
+                    to migrate your account to the{' '}
+                    <Anchor
+                      href="https://cosmik.network"
+                      target="_blank"
+                      fw={500}
+                      c={'blue'}
+                    >
+                      Cosmik Network
+                    </Anchor>
+                    .
+                  </Text>
+                </PopoverDropdown>
+              </Popover>
+            </Stack>
+          </Stack>
+          <Text fw={500} ta={'center'} c={'dark.1'}>
+            By continuing, you agree to our{' '}
+            <Anchor
+              component={Link}
+              href={'/privacy-policy'}
+              c="dark.2"
+              fw={600}
+            >
+              Privacy Policy
+            </Anchor>
+          </Text>
+        </Stack>
       </Center>
     </Container>
   );
--- src/webapp/app/(auth)/login/page.tsx
+++ src/webapp/app/(auth)/login/page.tsx
@@ -1,109 +1,11 @@
-'use client';
-
 import LoginForm from '@/features/auth/components/loginForm/LoginForm';
-import {
-  Stack,
-  Title,
-  Text,
-  Image,
-  Anchor,
-  Popover,
-  Button,
-  PopoverTarget,
-  PopoverDropdown,
-  Loader,
-  Badge,
-} from '@mantine/core';
-import { useEffect } from 'react';
-import { IoMdHelpCircleOutline } from 'react-icons/io';
-import SembleLogo from '@/assets/semble-logo.svg';
-import { useAuth } from '@/hooks/useAuth';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+import { verifySessionOnServer } from '@/lib/auth/dal.server';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
-  const { isAuthenticated, isLoading, refreshAuth } = useAuth();
-  const router = useRouter();
+export default async function Page() {
+  const session = await verifySessionOnServer();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      refreshAuth();
-      router.push('/home');
-    }
-  }, [isAuthenticated, router, refreshAuth]);
+  if (session) redirect('/home');
 
-  if (isAuthenticated || isLoading) {
-    return (
-      <Stack align="center">
-        <Loader type="dots" />
-      </Stack>
-    );
-  }
-
-  return (
-    <Stack gap={'xl'} align="center">
-      <Stack gap="xl" maw={300}>
-        <Stack gap={'xs'}>
-          <Stack align="center" gap={'xs'}>
-            <Image
-              src={SembleLogo.src}
-              alt="Semble logo"
-              w={48}
-              h={64.5}
-              mx={'auto'}
-            />
-            <Badge size="sm">Alpha</Badge>
-          </Stack>
-          <Title order={1} ta="center">
-            Welcome back
-          </Title>
-        </Stack>
-        <LoginForm />
-        <Stack align="center" gap={0}>
-          <Text fw={500} c={'stone'}>
-            {"Don't have an account? "}
-            <Anchor href="/signup" fw={500}>
-              Sign up
-            </Anchor>
-          </Text>
-          <Popover withArrow shadow="sm">
-            <PopoverTarget>
-              <Button
-                variant="transparent"
-                size="md"
-                fw={500}
-                fs={'italic'}
-                c={'gray'}
-                rightSection={<IoMdHelpCircleOutline size={22} />}
-              >
-                How your Cosmik Network account works
-              </Button>
-            </PopoverTarget>
-            <PopoverDropdown>
-              <Text fw={500} ta="center" maw={380}>
-                If you have a Bluesky account, you can sign in with it; no new
-                account is needed. In future, you will have the option to
-                migrate your account to the{' '}
-                <Anchor
-                  href="https://cosmik.network"
-                  target="_blank"
-                  fw={500}
-                  c={'blue'}
-                >
-                  Cosmik Network
-                </Anchor>
-                .
-              </Text>
-            </PopoverDropdown>
-          </Popover>
-        </Stack>
-      </Stack>
-      <Text fw={500} ta={'center'} c={'dark.1'}>
-        By continuing, you agree to our{' '}
-        <Anchor component={Link} href={'/privacy-policy'} c="dark.2" fw={600}>
-          Privacy Policy
-        </Anchor>
-      </Text>
-    </Stack>
-  );
+  return <LoginForm />;
 }
