19f7b44052b910fa3a09e2107471f75e214e9c04
pdelfan <pdelfan@gmail.com>
2026-03-03 17:03:14 -0800

    feat: use colour to indicate collection type

--- src/webapp/features/collections/components/collectionNavItem/CollectionNavItem.tsx
+++ src/webapp/features/collections/components/collectionNavItem/CollectionNavItem.tsx
@@ -1,12 +1,11 @@
 import { useNavbarContext } from '@/providers/navbar';
-import { Badge, NavLink, ThemeIcon } from '@mantine/core';
+import { Badge, NavLink } from '@mantine/core';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from './CollectionNavItem.module.css';
 import { isMarginUri } from '@/lib/utils/margin';
 import MarginLogo from '@/components/MarginLogo';
 import { CollectionAccessType } from '@semble/types';
-import { FaSeedling } from 'react-icons/fa6';
 
 interface Props {
   name: string;
@@ -21,32 +20,34 @@ export default function CollectionNavItem(props: Props) {
   const pathname = usePathname();
   const isActive = pathname === props.url;
   const isMargin = isMarginUri(props.uri);
+  const isOpenCollection = props.accessType === CollectionAccessType.OPEN;
 
   return (
     <NavLink
       component={Link}
       href={props.url}
       label={props.name}
-      variant="subtle"
+      active={isActive}
+      color="gray"
       classNames={{
         root: isActive
-          ? `${styles.navLink} ${styles.navLinkActive}`
-          : styles.navLink,
+          ? `${styles.navLink} ${isOpenCollection ? styles.navLinkActiveGreen : styles.navLinkActive}`
+          : isOpenCollection
+            ? `${styles.navLink} ${styles.navLinkGreen}`
+            : styles.navLink,
       }}
       onClick={toggleMobile}
-      leftSection={
-        isMargin ? (
-          <MarginLogo size={16} />
-        ) : props.accessType === CollectionAccessType.OPEN ? (
-          <ThemeIcon size={'sm'} variant="light" color={'green'} radius={'xl'}>
-            <FaSeedling size={10} />
-          </ThemeIcon>
-        ) : undefined
-      }
+      leftSection={isMargin ? <MarginLogo size={16} /> : undefined}
       rightSection={
         props.cardCount > 0 ? (
           <Badge
-            className={isActive ? styles.badgeActive : styles.badge}
+            className={
+              isActive
+                ? isOpenCollection
+                  ? styles.badgeActiveGreen
+                  : styles.badgeActive
+                : styles.badge
+            }
             circle
           >
             {props.cardCount}
