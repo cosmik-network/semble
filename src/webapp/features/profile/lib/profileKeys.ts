52bae29e06ffd3f9c4027693d7a9045d8eede71f
Wesley Finck <wesleyfinck@gmail.com>
2026-03-11 17:04:47 -0700

    update UI to include card and collection count on profile

--- src/webapp/features/profile/lib/profileKeys.ts
+++ src/webapp/features/profile/lib/profileKeys.ts
@@ -1,6 +1,6 @@
 export const profileKeys = {
   all: () => ['profiles'] as const,
-  profile: (didOrHandle: string) =>
-    [...profileKeys.all(), didOrHandle] as const,
+  profile: (didOrHandle: string, includeStats?: boolean) =>
+    [...profileKeys.all(), didOrHandle, { includeStats }] as const,
   mine: () => [...profileKeys.all(), 'mine'] as const,
 };
