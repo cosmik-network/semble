e31e3d9b7202bbdf204708c5690aee5210c89042
pdelfan <pdelfan@gmail.com>
2026-03-12 11:50:38 -0700

    feat: update collections navlist skeleton

--- src/webapp/features/collections/components/collectionsNavList/Skeleton.CollectionsNavList.tsx
+++ src/webapp/features/collections/components/collectionsNavList/Skeleton.CollectionsNavList.tsx
@@ -1,19 +1,25 @@
-import { NavLink, Skeleton, Stack } from '@mantine/core';
-import { BiCollection } from 'react-icons/bi';
+import { Group, Skeleton, Text, Stack } from '@mantine/core';
 
 export default function CollectionsNavListSkeleton() {
   return (
-    <NavLink
-      variant="subtle"
-      c="gray"
-      label="Collections"
-      leftSection={<BiCollection size={22} />}
-      opened={true}
-    >
-      <Stack gap={5} my={'sm'}>
-        <Skeleton h={40} w={'100%'} />
-        <Skeleton h={40} w={'100%'} />
+    <Stack gap={'xs'}>
+      <Group justify="space-between">
+        <Text fw={600} c={'gray'}>
+          Collections
+        </Text>
+
+        <Group gap={'xs'}>
+          <Skeleton radius={'xl'} h={26} w={26} />
+          <Skeleton radius={'xl'} h={26} w={26} />
+        </Group>
+      </Group>
+
+      <Stack gap={0}>
+        <Stack gap={5} my={'sm'}>
+          <Skeleton h={40} w={'100%'} />
+          <Skeleton h={40} w={'100%'} />
+        </Stack>
       </Stack>
-    </NavLink>
+    </Stack>
   );
 }
