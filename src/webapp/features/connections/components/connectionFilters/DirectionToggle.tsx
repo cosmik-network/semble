66735ce4e2a3c0d326716704277b458783c91cad
pdelfan <pdelfan@gmail.com>
2026-03-13 12:39:26 -0700

    feat: update direction toggle labels

--- src/webapp/features/connections/components/connectionFilters/DirectionToggle.tsx
+++ src/webapp/features/connections/components/connectionFilters/DirectionToggle.tsx
@@ -20,8 +20,8 @@ export default function DirectionToggle(props: Props) {
       onChange={handleChange}
       data={[
         { label: 'All', value: 'all' },
-        { label: 'Outgoing', value: 'outgoing' },
-        { label: 'Incoming', value: 'incoming' },
+        { label: 'To', value: 'outgoing' },
+        { label: 'From', value: 'incoming' },
       ]}
       radius={'xl'}
       withItemsBorders={false}
