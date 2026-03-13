28040e4ad73d612dbda4eb208699151ec0cd3e30
pdelfan <pdelfan@gmail.com>
2026-03-13 12:12:29 -0700

    feat: remove direction toggle borders

--- src/webapp/features/connections/components/connectionFilters/DirectionToggle.tsx
+++ src/webapp/features/connections/components/connectionFilters/DirectionToggle.tsx
@@ -24,6 +24,7 @@ export default function DirectionToggle(props: Props) {
         { label: 'Incoming', value: 'incoming' },
       ]}
       radius={'xl'}
+      withItemsBorders={false}
     />
   );
 }
