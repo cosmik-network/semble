37ce0f63c00444ff3f065d7c838020ab6e2d549a
pdelfan <pdelfan@gmail.com>
2026-04-01 16:42:19 -0700

    feat: update appearance segmented control skeleton

--- src/webapp/features/settings/containers/appearanceContainer/Skeleton.AppearanceContainer.tsx
+++ src/webapp/features/settings/containers/appearanceContainer/Skeleton.AppearanceContainer.tsx
@@ -6,9 +6,9 @@ export default function AppearanceContainerSkeleton() {
   return (
     <Container p="xs" size="xs">
       <Stack gap="xl">
-        <Skeleton w={'100%'} h={42} />
-        <Skeleton w={'100%'} h={42} />
-        <Skeleton w={'100%'} h={42} />
+        <Skeleton w={'100%'} h={36} />
+        <Skeleton w={'100%'} h={36} />
+        <Skeleton w={'100%'} h={36} />
       </Stack>
     </Container>
   );
