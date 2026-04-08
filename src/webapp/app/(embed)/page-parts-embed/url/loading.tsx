af85814a7d85446e09239cd1cc70515197fa2aaa
pdelfan <pdelfan@gmail.com>
2026-04-08 16:56:43 -0700

    feat: embed route skeletons

---
+++ src/webapp/app/(embed)/page-parts-embed/url/loading.tsx
@@ -0,0 +1,5 @@
+import UrlEmbedContainerSkeleton from '@/features/url/containers/urlEmbedContainer/skeleton.UrlEmbedContainer';
+
+export default function Loading() {
+  return <UrlEmbedContainerSkeleton />;
+}
