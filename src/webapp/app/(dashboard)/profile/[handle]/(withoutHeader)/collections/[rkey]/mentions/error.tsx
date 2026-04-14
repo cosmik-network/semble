32b64a3893feeac4e5cb1bd8e5a108d186151c91
pdelfan <pdelfan@gmail.com>
2026-04-13 17:27:25 -0700

    feat: error page on semble collection mentions

---
+++ src/webapp/app/(dashboard)/profile/[handle]/(withoutHeader)/collections/[rkey]/mentions/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import SembleMentionsContainerError from '@/features/semble/containers/sembleMentionsContainer/Error.SembleMentionsContainer';
+
+export default function Error() {
+  return <SembleMentionsContainerError />;
+}
