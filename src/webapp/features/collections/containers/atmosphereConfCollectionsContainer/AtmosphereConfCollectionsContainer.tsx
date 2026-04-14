f7567f3dad186849ed3cd7243e3ed850cbd7ea1d
pdelfan <pdelfan@gmail.com>
2026-04-14 13:45:35 -0700

    feat: collection sort on atmosphereConf collections page

--- src/webapp/features/collections/containers/atmosphereConfCollectionsContainer/AtmosphereConfCollectionsContainer.tsx
+++ src/webapp/features/collections/containers/atmosphereConfCollectionsContainer/AtmosphereConfCollectionsContainer.tsx
@@ -1,12 +1,20 @@
 'use client';
 
-import { Container, Stack } from '@mantine/core';
+import { Container, Group, Stack } from '@mantine/core';
 import AtmosphereConfCollectionsContainerContent from '../atmosphereConfCollectionsContainerContent/AtmosphereConfCollectionsContainerContent';
+import { CollectionFilters } from '../../components/collectionFilters/CollectionFilters';
 
 export default function AtmosphereConfCollectionsContainer() {
   return (
     <Container p="xs" size="xl">
       <Stack>
+        <Group gap={'xs'}>
+          <CollectionFilters.Root>
+            <CollectionFilters.SortSelect />
+            <CollectionFilters.ViewToggle />
+          </CollectionFilters.Root>
+        </Group>
+
         <AtmosphereConfCollectionsContainerContent />
       </Stack>
     </Container>
