4c09877c9762483108a84ce5cdd8022b9b037889
Wesley Finck <wesleyfinck@gmail.com>
2026-04-16 17:16:45 -0700

    include time card was created in profile card in added by tab

--- src/webapp/features/profile/components/profileCard/ProfileCard.tsx
+++ src/webapp/features/profile/components/profileCard/ProfileCard.tsx
@@ -1,15 +1,19 @@
 import { Avatar, Badge, Group, Stack, Text } from '@mantine/core';
 import { User } from '@semble/types';
 import { sanitizeText } from '@/lib/utils/text';
+import { getRelativeTime } from '@/lib/utils/time';
 import { ReactNode } from 'react';
 import { LinkCard } from '@/components/link/MantineLink';
 
 interface Props {
   profile: User;
   children?: ReactNode;
+  addedAt?: string;
 }
 
 export default function ProfileCard(props: Props) {
+  const time = props.addedAt ? getRelativeTime(props.addedAt) : null;
+
   return (
     <LinkCard
       withBorder
@@ -19,20 +23,36 @@ export default function ProfileCard(props: Props) {
       h={'100%'}
     >
       <Stack gap={'xxs'}>
-        <Group gap={'xs'} wrap="nowrap">
-          <Avatar
-            src={props.profile.avatarUrl?.replace('avatar', 'avatar_thumbnail')}
-            alt={`${props.profile.handle}'s avatar`}
-          />
+        <Group
+          gap={'xs'}
+          wrap="nowrap"
+          justify="space-between"
+          align="flex-start"
+        >
+          <Group gap={'xs'} wrap="nowrap">
+            <Avatar
+              src={props.profile.avatarUrl?.replace(
+                'avatar',
+                'avatar_thumbnail',
+              )}
+              alt={`${props.profile.handle}'s avatar`}
+            />
 
-          <Stack gap={0}>
-            <Text fw={600} c={'bright'} lineClamp={1}>
-              {sanitizeText(props.profile.name) || props.profile.handle}
+            <Stack gap={0}>
+              <Text fw={600} c={'bright'} lineClamp={1}>
+                {sanitizeText(props.profile.name) || props.profile.handle}
+              </Text>
+              <Text fw={600} c={'gray'} lineClamp={1}>
+                @{props.profile.handle}
+              </Text>
+            </Stack>
+          </Group>
+
+          {time && (
+            <Text c={'gray'} fz={'sm'}>
+              {time}
             </Text>
-            <Text fw={600} c={'gray'} lineClamp={1}>
-              @{props.profile.handle}
-            </Text>
-          </Stack>
+          )}
         </Group>
 
         <Group gap="xs">
