53c42df301b496cade0c0d1857168733d98a074a
pdelfan <pdelfan@gmail.com>
2026-04-17 14:39:08 -0700

    feat: "contributed to" layout

---
+++ src/webapp/app/(dashboard)/profile/[handle]/(withHeader)/network/contributed-to/layout.tsx
@@ -0,0 +1,40 @@
+import { getProfile } from '@/features/profile/lib/dal';
+import type { Metadata } from 'next';
+import { Fragment } from 'react';
+
+interface Props {
+  params: Promise<{ handle: string }>;
+  children: React.ReactNode;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { handle } = await params;
+  const profile = await getProfile(handle);
+
+  return {
+    title: `Collection ${profile.name} has contributed to`,
+    description: `Explore collections ${profile.name} has contributed to on Semble`,
+    authors: [
+      {
+        name: profile.name,
+        url: `${process.env.APP_URL}/profile/${handle}`,
+      },
+    ],
+    alternates: {
+      types: {
+        '': `at://${profile.id}`,
+      },
+    },
+    other: {
+      'atprotocol:creator': `at://${profile.id}`,
+    },
+  };
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout(props: Props) {
+  return <Fragment>{props.children}</Fragment>;
+}
