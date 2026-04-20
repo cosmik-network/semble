9744890c6660a7d7f38cd5c586822af2ffe60591
pdelfan <pdelfan@gmail.com>
2026-04-20 16:20:55 -0700

    feat: subtler semble stats

--- src/webapp/features/semble/components/sembleStats/SembleStats.tsx
+++ src/webapp/features/semble/components/sembleStats/SembleStats.tsx
@@ -1,6 +1,5 @@
-import { AvatarGroup, Divider, Group, Text, ThemeIcon } from '@mantine/core';
+import { Group, Text, ThemeIcon } from '@mantine/core';
 import { getLibrariesForUrl } from '../../lib/dal';
-import { LinkAvatar } from '@/components/link/MantineLink';
 import { getUrlMetadata } from '@/features/cards/lib/dal';
 import { BiCollection, BiLink } from 'react-icons/bi';
 import { MdOutlineStickyNote2 } from 'react-icons/md';
@@ -19,77 +18,54 @@ export default async function SembleStats(props: Props) {
   const data = await getLibrariesForUrl(props.url);
 
   return (
-    <Group gap={5}>
-      <Group gap={'xs'}>
-        <Group gap={5}>
-          {data.libraries.length > 0 ? (
-            <AvatarGroup>
-              {data.libraries.slice(0, 4).map((p, i) => (
-                <LinkAvatar
-                  key={i}
-                  href={`/profile/${p.user.handle}`}
-                  src={p.user.avatarUrl?.replace('avatar', 'avatar_thumbnail')}
-                  alt={p.user.handle}
-                  name={p.user.handle}
-                  size={'25'}
-                />
-              ))}
-            </AvatarGroup>
-          ) : (
-            <ThemeIcon variant="light" size={'sm'}>
-              <LuLibrary size={12} />
-            </ThemeIcon>
-          )}
+    <Group>
+      <Group gap={5}>
+        <ThemeIcon variant="transparent" color="cyan" size={'sm'}>
+          <LuLibrary />
+        </ThemeIcon>
 
-          <Text fw={500} fz={'sm'} c={'bright'}>
-            {stats?.libraryCount}
-          </Text>
-          <Text fw={500} fz={'sm'} c={'dimmed'}>
-            {stats?.libraryCount === 1 ? 'Save' : 'Saves'}
-          </Text>
-        </Group>
+        <Text fw={500} fz={'sm'} c={'bright'}>
+          {stats?.libraryCount}
+        </Text>
+        <Text fw={500} fz={'sm'} c={'dimmed'}>
+          {stats?.libraryCount === 1 ? 'Save' : 'Saves'}
+        </Text>
+      </Group>
 
-        <Divider orientation="vertical" />
+      <Group gap={5}>
+        <ThemeIcon variant="transparent" color="cyan" size={'sm'}>
+          <BiCollection />
+        </ThemeIcon>
+        <Text fw={500} fz={'sm'} c={'bright'}>
+          {stats?.collectionCount}
+        </Text>
+        <Text fw={500} fz={'sm'} c={'dimmed'}>
+          {stats?.collectionCount === 1 ? 'Collection' : 'Collections'}
+        </Text>
+      </Group>
 
-        <Group gap={5}>
-          <ThemeIcon variant="light" size={'sm'}>
-            <BiCollection size={12} />
-          </ThemeIcon>
-          <Text fw={500} fz={'sm'} c={'bright'}>
-            {stats?.collectionCount}
-          </Text>
-          <Text fw={500} fz={'sm'} c={'dimmed'}>
-            {stats?.collectionCount === 1 ? 'Collection' : 'Collections'}
-          </Text>
-        </Group>
+      <Group gap={5}>
+        <ThemeIcon variant="transparent" color="cyan" size={'sm'}>
+          <BiLink />
+        </ThemeIcon>
+        <Text fw={500} fz={'sm'} c={'bright'}>
+          {stats?.connections.all.total}
+        </Text>
+        <Text fw={500} fz={'sm'} c={'dimmed'}>
+          {stats?.connections.all.total === 1 ? 'Connection' : 'Connections'}
+        </Text>
+      </Group>
 
-        <Divider orientation="vertical" />
-
-        <Group gap={5}>
-          <ThemeIcon variant="light" size={'sm'}>
-            <BiLink size={12} />
-          </ThemeIcon>
-          <Text fw={500} fz={'sm'} c={'bright'}>
-            {stats?.connections.all.total}
-          </Text>
-          <Text fw={500} fz={'sm'} c={'dimmed'}>
-            {stats?.connections.all.total === 1 ? 'Connection' : 'Connections'}
-          </Text>
-        </Group>
-
-        <Divider orientation="vertical" />
-
-        <Group gap={5}>
-          <ThemeIcon variant="light" size={'sm'}>
-            <MdOutlineStickyNote2 size={12} />
-          </ThemeIcon>
-          <Text fw={500} fz={'sm'} c={'bright'}>
-            {stats?.noteCount}
-          </Text>
-          <Text fw={500} fz={'sm'} c={'dimmed'}>
-            {stats?.noteCount === 1 ? 'Note' : 'Notes'}
-          </Text>
-        </Group>
+      <Group gap={5}>
+        <ThemeIcon variant="transparent" color="cyan" size={'sm'}>
+          <MdOutlineStickyNote2 />
+        </ThemeIcon>
+        <Text fw={500} fz={'sm'} c={'bright'}>
+          {stats?.noteCount}
+        </Text>
+        <Text fw={500} fz={'sm'} c={'dimmed'}>
+          {stats?.noteCount === 1 ? 'Note' : 'Notes'}
+        </Text>
       </Group>
     </Group>
   );
