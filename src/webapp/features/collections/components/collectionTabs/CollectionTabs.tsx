77f6297298519750fa18cccf5c9bc3d0ce336d86
pdelfan <pdelfan@gmail.com>
2026-04-22 14:04:24 -0700

    feat: collection tabs stats

--- src/webapp/features/collections/components/collectionTabs/CollectionTabs.tsx
+++ src/webapp/features/collections/components/collectionTabs/CollectionTabs.tsx
@@ -2,8 +2,12 @@
 
 import { Group, Paper, Scroller, Tabs } from '@mantine/core';
 import { usePathname } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 import TabItem from './TabItem';
 import ContributorTab from './ContributorTab';
+import { getCollectionPageByAtUri } from '../../lib/dal';
+import { collectionKeys } from '../../lib/collectionKeys';
+import useUrlMetadata from '@/features/cards/lib/queries/useUrlMetadata';
 
 interface Props {
   handle: string;
@@ -16,13 +20,36 @@ export default function CollectionTabs(props: Props) {
   const currentTab = segment || 'cards'; // treat base route as 'cards'
   const basePath = `/profile/${props.handle}/collections/${props.rkey}`;
 
+  const collectionUrl = `${process.env.NEXT_PUBLIC_APP_URL ?? 'http://127.0.0.1:4000'}/profile/${props.handle}/collections/${props.rkey}`;
+
+  const { data: collection } = useQuery({
+    queryKey: [...collectionKeys.all(), 'stats', props.rkey],
+    queryFn: () =>
+      getCollectionPageByAtUri({
+        recordKey: props.rkey,
+        handle: props.handle,
+        params: { limit: 1 },
+      }),
+  });
+
+  const { data: urlMetadata } = useUrlMetadata({
+    url: collectionUrl,
+    includeStats: true,
+  });
+
+  const stats = urlMetadata?.stats;
+
   return (
     <Tabs value={currentTab}>
       <Paper radius={0}>
         <Tabs.List>
           <Scroller>
             <Group wrap="nowrap">
-              <TabItem value="cards" href={basePath}>
+              <TabItem
+                value="cards"
+                href={basePath}
+                count={collection?.cardCount}
+              >
                 Cards
               </TabItem>
               <TabItem value="similar-cards" href={`${basePath}/similar-cards`}>
@@ -31,10 +58,18 @@ export default function CollectionTabs(props: Props) {
               <TabItem value="mentions" href={`${basePath}/mentions`}>
                 Mentions
               </TabItem>
-              <TabItem value="connections" href={`${basePath}/connections`}>
+              <TabItem
+                value="connections"
+                href={`${basePath}/connections`}
+                count={stats?.connections?.all?.total}
+              >
                 Connections
               </TabItem>
-              <TabItem value="followers" href={`${basePath}/followers`}>
+              <TabItem
+                value="followers"
+                href={`${basePath}/followers`}
+                count={collection?.followerCount}
+              >
                 Followers
               </TabItem>
               <ContributorTab
@@ -42,7 +77,11 @@ export default function CollectionTabs(props: Props) {
                 rkey={props.rkey}
                 basePath={basePath}
               />
-              <TabItem value="added-by" href={`${basePath}/added-by`}>
+              <TabItem
+                value="added-by"
+                href={`${basePath}/added-by`}
+                count={stats?.libraryCount}
+              >
                 Added by
               </TabItem>
             </Group>
--- src/webapp/features/collections/components/collectionTabs/ContributorTab.tsx
+++ src/webapp/features/collections/components/collectionTabs/ContributorTab.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { Suspense } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import TabItem from './TabItem';
 import useCollection from '../../lib/queries/useCollection';
+import { getCollectionContributors } from '../../lib/dal';
+import { collectionKeys } from '../../lib/collectionKeys';
 import { CollectionAccessType } from '@semble/types';
 
 interface Props {
@@ -19,12 +22,25 @@ function ContributorTabInner(props: Props) {
 
   const collection = data.pages[0];
 
+  const { data: contributors } = useQuery({
+    queryKey: [
+      ...collectionKeys.collection(collection.id),
+      'contributors-count',
+    ],
+    queryFn: () => getCollectionContributors(collection.id, { limit: 1 }),
+    enabled: collection.accessType === CollectionAccessType.OPEN,
+  });
+
   if (collection.accessType !== CollectionAccessType.OPEN) {
     return null;
   }
 
   return (
-    <TabItem value="contributors" href={`${props.basePath}/contributors`}>
+    <TabItem
+      value="contributors"
+      href={`${props.basePath}/contributors`}
+      count={contributors?.pagination.totalCount}
+    >
       Contributors
     </TabItem>
   );
