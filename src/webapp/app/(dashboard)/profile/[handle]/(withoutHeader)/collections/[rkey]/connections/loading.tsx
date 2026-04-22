38ae1c5b5c4a4250c3ca86d256d26fda7e90ff62
pdelfan <pdelfan@gmail.com>
2026-04-22 15:46:54 -0700

    fix: collection tab and content spacing

--- src/webapp/app/(dashboard)/profile/[handle]/(withoutHeader)/collections/[rkey]/connections/loading.tsx
+++ src/webapp/app/(dashboard)/profile/[handle]/(withoutHeader)/collections/[rkey]/connections/loading.tsx
@@ -1,5 +1,10 @@
 import ConnectionsContainerSkeleton from '@/features/connections/containers/connectionsContainer/Skeleton.ConnectionsContainer';
+import { Container } from '@mantine/core';
 
 export default function Loading() {
-  return <ConnectionsContainerSkeleton />;
+  return (
+    <Container p="xs" size="xl">
+      <ConnectionsContainerSkeleton />
+    </Container>
+  );
 }
