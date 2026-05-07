a115dea18392a8cab6c1b9ccde35ae4f13456d72
Pouria Delfanazari <pdelfan@gmail.com>
2026-05-07 11:25:21 -0700

    Update Skeleton.RecentCollections.tsx

--- src/webapp/features/home/components/recentCollections/Skeleton.RecentCollections.tsx
+++ src/webapp/features/home/components/recentCollections/Skeleton.RecentCollections.tsx
@@ -17,9 +17,9 @@ export default function RecentCollectionsSkeleton() {
       </Group>
 
       <Group gap="xs">
-        <Skeleton w={56} h={30} radius={'xl'} />
         <Skeleton w={82} h={30} radius={'xl'} />
         <Skeleton w={110} h={30} radius={'xl'} />
+        <Skeleton w={56} h={30} radius={'xl'} />
       </Group>
 
       <SimpleGrid cols={{ base: 1, sm: 2, lg: 4 }} spacing="xs">
