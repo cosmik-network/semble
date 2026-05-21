618a6a9038626ead0d54135344c58e2955a0fdb1
pdelfan <pdelfan@gmail.com>
2026-05-21 15:22:29 -0700

    feat: move share button to header

--- src/webapp/app/(dashboard)/url/layout.tsx
+++ src/webapp/app/(dashboard)/url/layout.tsx
@@ -1,6 +1,7 @@
 import BackButton from '@/components/navigation/backButton/BackButton';
 import Header from '@/components/navigation/header/Header';
 import ReaderHeaderButton from '@/features/reader/components/ReaderHeaderButton/ReaderHeaderButton';
+import ShareHeaderButton from '@/features/semble/components/ShareHeaderButton/ShareHeaderButton';
 import { Fragment, Suspense } from 'react';
 
 interface Props {
@@ -14,6 +15,9 @@ export default async function Layout(props: Props) {
         <BackButton />
         <Suspense fallback={null}>
           <ReaderHeaderButton />
+        </Suspense>
+        <Suspense fallback={null}>
+          <ShareHeaderButton />
         </Suspense>
       </Header>
       {props.children}
